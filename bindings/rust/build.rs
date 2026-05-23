281d28e4168436f1f5a852f3640c5f62ae6c8ad4
Lee0701 <hgl1002@naver.com>
2026-05-23 15:25:19 +0900

    Fix link search path

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,4 +1,8 @@
+use std::{env, path::Path};
+
 fn main() {
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let lib_path = Path::new(&manifest_dir).join("../../build/hangul/Release");
     println!("cargo:rustc-link-lib=hangul");
-    println!("cargo:rustc-link-search=native=libhangul/build/hangul/Release");
+    println!("cargo:rustc-link-search=native={}", lib_path.display());
 }
